43effd199ac3da6af3973b57a09cb80aed9a05d0
Raghuram Subramani <raghus2247@gmail.com>
2021-11-08 20:59:37 +0530

    finished gitcloneuser.py

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # UsefulScripts
 Just a couple scripts I use.
+
+### gitcloneuser.py
+
+Clones all **public** github repositories of a user.
+
+Note: This requires git to be installed and in path.
+
+example:
+    
+    ```bash
+    python3 gitcloneuser.py -u stagefright071
+    ```
---
+++ gitcloneuser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import requests
+import json
+import os
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Clone all repositories from a github user.')
+parser.add_argument('-u','--user', help='Specify github user.', required=True)
+args = vars(parser.parse_args())
+
+user = args['user']
+
+repos_list = json.loads(requests.get(f'https://api.github.com/users/{user}/repos').text)
+
+for i in range(0,len(repos_list)):
+    try:
+        url = repos_list[i]['html_url']
+    except:
+        print('An error occoured. Please check the username again.')
+        sys.exit(1)
+    os.system(f'git clone {url}')
